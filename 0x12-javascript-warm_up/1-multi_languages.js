f9f5633981597e0c62004516021c71964d6d76ca
Boyannie <117803536+Boyannie@users.noreply.github.com>
2023-10-06 10:18:59 +0300

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/1-multi_languages.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-const CPrint = 'C is fun';
-const PythonPrint = 'Python is cool';
-const JavascriptPrint = 'JavaScript is amazing';
-console.log(CPrint);
-console.log(PythonPrint);
-console.log(JavascriptPrint);
--- 0x12-javascript-warm_up/13-add.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-exports.add = function (number1, number2) {
-  const suma = number1 + number2;
-  return (suma);
-};
